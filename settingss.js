5f0628f80b77325c76d27906ed553b5780d3ea2f
djkilostygh <162448794+djkilostygh@users.noreply.github.com>
2025-08-16 02:59:09 +0000

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~ZqdWVDaJ#wW5G_BfD06YxYTrq6Qu8JHKVJnW-l7MIX48wycpnyVI",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "233550445739", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
